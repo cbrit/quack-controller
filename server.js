1f91cf6304a0b2816fb49cdb5e03364b1617f726
Collin Brittain <cbrit@Collins-MacBook-Pro.local>
2018-12-04 16:26:38 -0600

    Include simple timestamp in messages

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const io = require('socket.io')(3001);
 
+const qtime = require('./lib/qtime.js')
+
 io.on('connection', function(socket) {
     console.log('Controller: Connection received')
 
-    socket.on('chat message', function(msg) {
-        io.emit('chat message', msg);
+    socket.on('chat message', function(content) {
+        var message = qtime.timestamp() + ": " + content;
+
+        io.emit('chat message', message);
     });
 });
